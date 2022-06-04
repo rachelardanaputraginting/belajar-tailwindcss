19d1e60bd64a8eb8166d1985b17106eb0ce0bb89
Rachel Ardana Putra Ginting <rachelardanaputraginting@gmail.com>
2022-06-04 11:32:42 +0700

    belajar tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      fontFamily: { 
+        quick:['Quicksand'], 
+      },
+    },
+  },
+  plugins: [],
+}
